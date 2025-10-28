8a1dbba13ab5329fc6cc50b5bea3bbc4775231d4
Соня <vnukova0904@gmail.com>
2025-10-29 01:32:35 +0300

    Add files via upload

---
+++ src/lib/text2.py
@@ -0,0 +1,25 @@
+def normalize(text: str, *, casefold: bool = True, yo2e: bool = True):
+    if casefold:
+        text=text.casefold()
+    if yo2e:
+        text=text.replace('ё','е').replace('Ё','Е')
+    if '\t' in text or '\n' in text or '\r' in text:
+        text=text.replace('\t',' ').replace('\n',' ').replace('\r',' ')
+    text=' '.join(text.split())
+    return text
+
+import re
+def tokenize(text):
+    pat=r'[^\w-]'
+    text= re.sub(pat,'  ',text)
+    return text.split()
+
+def count_freq(tokens: list[str]):
+    counts=dict()
+    for i in (sorted(set(tokens))):
+            counts[i] = tokens.count(i)
+    return counts
+
+def top_n(freq: dict[str, int], n: int = 5):
+     sorted_freq = sorted(freq.items(), key=lambda x: (-x[1], x[0]))
+     return  sorted_freq [:n]
